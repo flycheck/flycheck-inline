816d37bd7f7034502a7569515df1898a3bcd155c
fmdkdd <fmdkdd@0xc0de.fr>
2018-05-29 15:14:23 +0200

    Fix Setup header in commentary section

--- flycheck-inline.el
+++ flycheck-inline.el
@@ -27,9 +27,9 @@
 
 ;; Provide an error display function to show Flycheck errors inline, directly
 ;; below their location in the buffer.
-
-;;;; Setup
-
+;;
+;; # Setup
+;;
 ;; (with-eval-after-load 'flycheck
 ;;   (flycheck-inline-mode))
 
